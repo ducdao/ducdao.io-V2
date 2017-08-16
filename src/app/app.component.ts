bc40154dad0d49514b3d1c2df2fa09e51d791efd
Duc Dao <ducdao11@gmail.com>
2017-08-16 00:11:35 -0700

    Added ng2-responsive that lets me handle HTML/CSS for desktop and mobile. Added mobile home page.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,11 +13,4 @@ export class AppComponent {
       {pageName: 'PERSONAL', link: '/personal'},
       {pageName: 'CONTACT',  link: '/contact'}
     ];
-
-    tiles = [
-    {text: 'One', cols: 3, rows: 1, color: 'lightblue'},
-    {text: 'Two', cols: 1, rows: 2, color: 'lightgreen'},
-    {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
-    {text: 'Four', cols: 2, rows: 1, color: '#DDBDF1'},
-  ];
  }
