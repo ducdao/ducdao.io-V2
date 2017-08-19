11e24f0c9eca667433b13cfb76376ffc9467333f
Duc Dao <ducdao11@gmail.com>
2017-08-18 17:40:48 -0700

    Imported HttpModule.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ResponsiveModule } from 'ng2-responsive';
+import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -40,6 +41,7 @@ import { MdButtonModule,
     BrowserModule,
     BrowserAnimationsModule,
     ResponsiveModule,
+    HttpModule,
     AppRoutingModule,
     MdButtonModule,
     MdMenuModule,
