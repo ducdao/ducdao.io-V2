4ecfc1fe820fa6d385346c39ecaa19b370ef333a
Duc Dao <ducdao11@gmail.com>
2017-12-22 15:21:48 -0800

    Fixed persisting pink background when clicking a link in nav bar in css of theme (line 1507).

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  cards:any;
+  cards: any;
 
   constructor(private cardsService: GetCardsService) { }
 
